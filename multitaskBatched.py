c3e6818e3ae67f1421bd3fdf9bd28fbfb72b9279
ianhill60 <71084427+ianhill60@users.noreply.github.com>
2021-10-01 10:13:54 -0400

    Update multitaskBatched.py
    Apply i2 data to an i4-trained model - report R^2 and MAEs for both test sets (i4 and i2).

--- multitaskBatched.py
+++ multitaskBatched.py
@@ -1,122 +1,185 @@
-import numpy as np
-import pandas as pd
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import numpy as np
+import pandas as pd
+import os
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 from torch.utils.data.dataset import random_split
 import gpytorch
+import sklearn
 from sklearn.model_selection import train_test_split
-
+import tqdm
+from scipy.cluster.vq import kmeans2
 
 class StandardScaler:
-    def fit(self, x):
-        self.mean = x.mean(0, keepdim=True)
-        self.std = x.std(0, unbiased=False, keepdim=True)
-
-    def transform(self, x):
-        x -= self.mean
-        x /= (self.std + 1e-7)
-        return x
-
-    def inverse_transform_mean(self, x):
+  def fit(self, x):
+    self.mean = x.mean(0, keepdim=True)
+    self.std = x.std(0, unbiased=False, keepdim=True)
+  def transform(self, x):
+    x -= self.mean
+    x /= (self.std + 1e-7)
+    return x
+  def inverse_transform_mean(self, x):
         '''To transform mean'''
         return x * (self.std + 1e-7) + self.mean
-
-    def inverse_transform_std(self, x):
+  def inverse_transform_std(self, x):
         '''To transform standard deviation'''
         return x * (self.std + 1e-7)
 
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-xdata = pd.read_csv('xdf.csv', index_col=0) #update with path
-ydata = pd.read_csv('ydf.csv', index_col=0) #update with path
+num_inducing_pts = 1000            # Number of inducing points in each hidden layer
+num_tasks = 3
+num_latents = 4 # how many linear functions are learned to correlate tasks. 
+xdata = pd.read_csv('xdf.csv',index_col=0)
+ydata = pd.read_csv('ydf.csv',index_col=0)
+x2data = torch.tensor(np.array(pd.read_csv('xdfi2.csv',index_col=0)))
+y2data = torch.tensor(np.array(pd.read_csv('ydfi2.csv',index_col=0)))
 xdat = np.array(xdata)
 ydat = np.array(ydata)
 xtr, xte, ytr, yte = train_test_split(xdat, ydat, test_size=0.2, random_state=17)
 scaler = StandardScaler()
 scaler1 = StandardScaler()
+scaler2 = StandardScaler()
+scaler3 = StandardScaler()
 xtr1 = torch.tensor(xtr)
 xte1 = torch.tensor(xte)
 ytr1 = torch.tensor(ytr)
 yte1 = torch.tensor(yte)
 scaler.fit(xtr1)
 scaler1.fit(ytr1)
+scaler2.fit(x2data)
+scaler3.fit(y2data)
 train_x = scaler.transform(xtr1).double()
 test_x = scaler.transform(xte1).double()
 train_y = scaler1.transform(ytr1).double()
 test_y = scaler1.transform(yte1).double()
+train_n = len(train_x) 
+testi2_x = scaler2.transform(x2data).double()
+testi2_y = scaler3.transform(y2data).double()
 
 train_dataset = TensorDataset(train_x, train_y)
 val_dataset = TensorDataset(test_x, test_y)
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=20, drop_last=True)
-val_loader = DataLoader(dataset=val_dataset, batch_size=20, drop_last=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=1000, drop_last=False)
+val_loader = DataLoader(dataset=val_dataset, batch_size=1000, drop_last=False)
+
+class MultitaskGPModel(gpytorch.models.ApproximateGP):
+    def __init__(self):
+
+        # Let's use a different set of inducing points for each latent function
+        # Use k-means to initialize inducing points (only helpful for the first layer)
+        inducing_points = (train_x[torch.randperm(min(1000 * 100, train_n))[0:num_inducing_pts], :])
+        inducing_points = inducing_points.clone().data.cpu().numpy()
+        inducing_points = torch.tensor(kmeans2(train_x.data.cpu().numpy(),
+                               inducing_points, minit='matrix')[0])
+
+        if torch.cuda.is_available():
+          inducing_points = inducing_points.cuda()
 
 
-class MultitaskGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood):
-        super(MultitaskGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.MultitaskMean(
-            gpytorch.means.ConstantMean(), num_tasks=3
+        # We have to mark the CholeskyVariationalDistribution as batch
+        # so that we learn a variational distribution for each task
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+            inducing_points.size(-2), batch_shape=torch.Size([num_latents])
         )
-        self.covar_module = gpytorch.kernels.MultitaskKernel(
-            gpytorch.kernels.RBFKernel(), num_tasks=3, rank=1
+        
+        # We have to wrap the VariationalStrategy in a LMCVariationalStrategy
+        # so that the output will be a MultitaskMultivariateNormal rather than a batch output
+        variational_strategy = gpytorch.variational.LMCVariationalStrategy(
+            gpytorch.variational.VariationalStrategy(
+                self, inducing_points, variational_distribution, learn_inducing_locations=True
+            ),
+            num_tasks=num_tasks,
+            num_latents=num_latents,
+            latent_dim=-1
         )
-
+        
+        super().__init__(variational_strategy)
+        
+        # The mean and covariance modules should be marked as batch
+        # so we learn a different set of hyperparameters
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([num_latents]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latents])),
+            batch_shape=torch.Size([num_latents])
+        )
+        
     def forward(self, x):
+        # The forward function should be written as if we were dealing with each output
+        # dimension in batch
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-
-# Estimate a and b
 torch.manual_seed(42)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-x_batch1 = train_x[0:20]
-y_batch1 = train_y[0:20]
+model = MultitaskGPModel().double().to(device)
 likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3).to(device)
-model = MultitaskGPModel(x_batch1, y_batch1, likelihood).to(device).double()
-# "Loss" for GPs - the marginal log likelihood
-mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-# optimizer = optim.SGD(model.parameters(), lr=1e-1)
-optimizer = optim.Adam(model.parameters(), lr=0.5)  # Includes GaussianLikelihood parameters
 
-n_epochs = 50
-training_losses = []
-validation_losses = []
-# print(model.state_dict())
+#smoke_test = ('CI' in os.environ)
+num_epochs = 10 #if smoke_test else 500
 
-for epoch in range(n_epochs):
-    batch_losses = []
-    for x_batch, y_batch in train_loader:
+
+model.train()
+likelihood.train()
+
+optimizer = torch.optim.Adam([
+    {'params': model.parameters()},
+    {'params': likelihood.parameters()},
+], lr=.01)
+
+# Our loss object. We're using the VariationalELBO, which essentially just computes the ELBO
+mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=train_y.size(0)).cuda()
+
+# We use more CG iterations here because the preconditioner introduced in the NeurIPS paper seems to be less
+# effective for VI.
+epochs_iter = tqdm.tqdm_notebook(range(num_epochs), desc="Epoch")
+val_loss = []
+for i in epochs_iter:
+  batch_losses = []
+  for x_batch, y_batch in train_loader:
+    x_batch = x_batch.to(device)
+    y_batch = y_batch.to(device)
+    # Within each iteration, we will go over each minibatch of data
+    optimizer.zero_grad()
+    output = model(x_batch)
+    loss = -mll(output, y_batch)
+    epochs_iter.set_postfix(loss=loss.item())
+    loss.backward()
+    optimizer.step()
+  
+    batch_losses.append(loss)
+
+ 
+# Set into eval mode
+model.eval().to(device)
+likelihood.eval().to(device)
+
+# Make predictions
+means = torch.empty((1,3)).to(device)
+means1 = torch.empty((1,3)).to(device)
+
+with torch.no_grad():
+    for x_batch, y_batch in val_loader:
         x_batch = x_batch.to(device)
         y_batch = y_batch.to(device)
-        model.train()
-        likelihood.train()
-        output = model(x_batch)
-        loss = -mll(output, y_batch)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-        batch_losses.append(loss)
-        print(batch_losses)
-    training_loss = np.mean(batch_losses)
-    training_losses.append(training_loss)
+        preds = likelihood(model(x_batch))
+        means = torch.cat([means, preds.mean])
+    testi2_x = testi2_x.to(device)
+    preds1 = likelihood(model(testi2_x))
+    means1 = torch.cat([means1, preds1.mean])
 
-    with torch.no_grad():
-        val_losses = []
-        for x_val, y_val in val_loader:
-            x_val = x_val.to(device)
-            y_val = y_val.to(device)
-            model.eval()
-            yhat = model(x_val)
-            val_loss = -mll(yhat, y_val)
-            val_losses.append(val_loss)
-        validation_loss = np.mean(val_losses)
-        validation_losses.append(validation_loss)
+means = means[1:]
+means = means.to('cpu')
+means1 = means1[1:]
+means1 = means1.to('cpu')
 
-    print(f"[{epoch + 1}] Training loss: {training_loss:.3f}\t Validation loss: {validation_loss:.3f}")
+testi2_y = testi2_y.to('cpu')
+test_y = test_y.to('cpu')
 
-print(model.state_dict())
+print(sklearn.metrics.mean_absolute_error(scaler1.inverse_transform_mean(test_y), scaler1.inverse_transform_mean(means), multioutput='raw_values'))
+print(sklearn.metrics.r2_score(scaler1.inverse_transform_mean(test_y), scaler1.inverse_transform_mean(means)))
+
+print(sklearn.metrics.mean_absolute_error(scaler3.inverse_transform_mean(testi2_y), scaler3.inverse_transform_mean(means1), multioutput='raw_values'))
+print(sklearn.metrics.r2_score(scaler3.inverse_transform_mean(testi2_y), scaler3.inverse_transform_mean(means1)))
